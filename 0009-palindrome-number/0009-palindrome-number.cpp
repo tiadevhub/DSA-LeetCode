c8da97dbc79b9f0a879f35ac8dcfb5e2f79ef08e
Tiya <tiya2892.beai25@chitkara.edu.in>
2026-07-05 14:11:44 +0530

    Time: 5 ms (19.89%), Space: 8.5 MB (92.56%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+        long long rev=0;
+        int org=x;
+        while(x!=0){
+            int digit=x%10;
+            rev=rev*10+digit;
+            x/=10;
+        }
+        if(rev==org){
+            return true;
+        }else{
+            return false;
+        }
+        
+    }
+};
