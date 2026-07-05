95849e4a2138dcffe166b8071a9fc64ed81eb90c
Tiya <tiya2892.beai25@chitkara.edu.in>
2026-07-05 14:40:52 +0530

    Time: 1 ms (97.9%), Space: 42.5 MB (79.59%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int addDigits(int num) {
+        while(num>=10){
+            int sum=0;
+            while(num!=0){
+                int d=num%10;
+                sum+=d;
+                num/=10;
+
+            }
+            num=sum;
+        }
+        return num;
+        
+    }
+}
