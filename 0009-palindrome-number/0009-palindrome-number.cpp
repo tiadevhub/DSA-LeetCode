e19bc4aeb0d3ad8a1b7e695e4bdb91c71250fd4e
Tiya <tiya2892.beai25@chitkara.edu.in>
2026-07-05 14:17:10 +0530

    Time: 0 ms (100%), Space: 8.6 MB (65.6%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -4,11 +4,11 @@ public:
         if(x<0){
             return false;
         }
-        long long rev=0;
         int org=x;
+        long long rev=0;
         while(x!=0){
             int digit=x%10;
-            rev=rev*10+digit;
+             rev=rev*10+digit;
             x/=10;
         }
         if(rev==org){
