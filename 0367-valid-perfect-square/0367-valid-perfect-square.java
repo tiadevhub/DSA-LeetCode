1adf1158b91e0629657216f6b3b5858fb30e2883
Tiya <tiya2892.beai25@chitkara.edu.in>
2026-07-06 09:36:09 +0530

    Time: 2 ms (17%), Space: 41.8 MB (87.55%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.java
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean isPerfectSquare(int num) {
-        for(long  i=1;i*i<=num;i++){
+        for(long i=1;i*i<=num;i++){
             if(i*i==num){
                 return true;
             }
