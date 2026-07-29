e9891e2b51c07b92827653efa8dcb980d6626773
Tiya <tiya2892.beai25@chitkara.edu.in>
2026-07-29 23:16:04 +0530

    Time: 8 ms (52.47%), Space: 44.2 MB (47.92%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        if(s1.length()>s2.length()){
+            return false;
+        }
+        int f1[]=new int[26];
+        int f2[]=new int[26];
+
+        for(int i=0;i<s1.length();i++){
+            f1[s1.charAt(i)-'a']++;
+            f2[s2.charAt(i)-'a']++;
+        }
+        if(Arrays.equals(f1,f2)){
+            return true;
+        }
+        for(int i=s1.length();i<s2.length();i++){
+            f2[s2.charAt(i)-'a']++;
+            f2[s2.charAt(i-s1.length())-'a']--;
+
+        
+        if(Arrays.equals(f1,f2)){
+            return true;
+        }
+        }
+        return false;
+
+        
+
+    }
+}
