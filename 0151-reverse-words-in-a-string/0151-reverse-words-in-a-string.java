7a64d5baefb8d586b982fc3158a577347286fc01
Tiya <tiya2892.beai25@chitkara.edu.in>
2026-08-04 12:41:12 +0530

    Time: 7 ms (63.47%), Space: 43.8 MB (97.19%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String reverseWords(String s) {
+        StringBuilder sb=new StringBuilder();
+        String arr[]=s.trim().split("\\s+");
+        for(int i=arr.length-1;i>=0;i--){
+            sb.append(arr[i]);
+            if(i!=0){
+                sb.append(" ");
+            }
+        }
+        return sb.toString();
+    }
+}
